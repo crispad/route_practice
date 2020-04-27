bc67322c110f32d1472f525bc039b2fa3c445388
Cristina Laryea <crispad06@gmail.com>
2020-04-26 21:29:03 -0400

    created image folder and added pic to landing page

--- route/src/components/Landing.js
+++ route/src/components/Landing.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Thinking from '../images/thinking.png';
 
 
 function Landing() {
   return (
     <div>
       <h1>Welcome to the Landing Page of Route Practice</h1>
+        <img src={Thinking} alt='thinking' />
     </div>
   );
 }
