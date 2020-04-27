90a0ffd1e833db9fb5239018e6713ae185f1b596
Cristina Laryea <crispad06@gmail.com>
2020-04-26 20:41:12 -0400

    created landing and navbar components

---
+++ route/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Landing() {
+  return (
+    <div>
+      <h1>Welcome to the Landing Page of Route Practice</h1>
+    </div>
+  );
+}
+
+export default Landing;
